1795312b94aa0e245b3ac8fb2037ee48ed75377b
Rafal <rafalmika133@gmail.com>
2022-03-02 19:04:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroductionToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
