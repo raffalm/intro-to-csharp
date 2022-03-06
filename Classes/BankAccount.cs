dd8ef64491b577da515613061592a73a73efec64
Rafal <rafalmika133@gmail.com>
2022-03-06 16:48:46 +0100

    Introduction to classes completed

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCSharp.Classes
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        {
+            get 
+            {
+                decimal balance = 0;
+                foreach (var transaction in allTransactions)
+                    balance += transaction.Amount;
+
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0) throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposite must be positive");
+            allTransactions.Add(new Transaction(amount,date,note));
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0) throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            if(Balance-amount<0) throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            allTransactions.Add(new Transaction(-amount, date, note));
+                
+        }
+        public string GetAcountHistory()
+        {
+            StringBuilder report = new StringBuilder();
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach(var transaction in allTransactions)
+            {
+                balance += transaction.Amount;
+                report.AppendLine($"{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t{balance}\t{transaction.Note}");
+            }
+            return report.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace IntroductionToCSharp
+namespace IntroductionToCSharp.Classes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("rafal", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner}");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.Write(account.GetAcountHistory());
+
+            // Test that the initial balances must be positive.
+            /*BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }*/
+            // Test for a negative balance.
+           /* try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }*/
+
         }
     }
 }
